5473a46771b71ac31d50b4ea4de6440dc12f7110
preprider2 <cale@stockmilster.com>
2026-05-25 11:14:14 -0500

    Create MapView component

---
+++ src/components/map/MapView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MapView: React.FC = () => {
+  return (
+    <div className="map-view">
+      {/* Map component content will go here */}
+    </div>
+  );
+};
+
+export default MapView;
